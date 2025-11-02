f930ae80942f1a3e75923cc275dbb39e1443f0de
nayanashaji <nayanashaji17102005@gmail.com>
2025-11-02 21:36:53 +0530

    feat: Cart page with add to cart, go to cart, remove from cart

--- ecommerce_cart/cart.js
+++ ecommerce_cart/cart.js
@@ -1,2 +1,14 @@
+import { createProductCard } from "./createProductCard.js";
+import { findProductInCart } from "./findProductInCart.js";
+
 let cartContainer=document.getElementById("cart");
-let cart=[];
+let cart=JSON.parse(localStorage.getItem("cart"))||[];
+
+cartContainer.addEventListener("click",(e)=>{
+    cart=cart.filter(({_id})=>_id!==e.target.dataset.id);
+    cartContainer.innerHTML="";
+    createProductCard(cart,cartContainer,findProductInCart,"cart");
+    localStorage.setItem("cart",JSON.stringify(cart));
+});
+
+createProductCard(cart,cartContainer,findProductInCart,"cart");
