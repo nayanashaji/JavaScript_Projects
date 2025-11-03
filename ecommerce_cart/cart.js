2ad944c16307e03000300e29e43b58b9ce905b44
nayanashaji <nayanashaji17102005@gmail.com>
2025-11-03 22:57:00 +0530

    Feat: Added cart price details

--- ecommerce_cart/cart.js
+++ ecommerce_cart/cart.js
@@ -11,4 +11,22 @@ cartContainer.addEventListener("click",(e)=>{
     localStorage.setItem("cart",JSON.stringify(cart));
 });
 
+const cartLength=document.querySelector(".item-count");
+cartLength.innerText=JSON.parse(localStorage.getItem("cart")).length;
+
+const productPrice=document.querySelector(".product-price");
+const finalPrice=JSON.parse(localStorage.getItem("cart")).reduce((acc,cur)=>acc+cur.newPrice,0);
+productPrice.innerText=finalPrice;
+
+const discount=document.querySelectorAll(".discounted-amount");
+const priceBeforeDiscount=JSON.parse(localStorage.getItem("cart")).reduce((acc,cur)=>acc+cur.oldPrice,0);
+const discountAmount=priceBeforeDiscount-finalPrice;
+for(let element of discount)
+{
+    element.innerText=discountAmount;
+}
+
+const totalAmount=document.querySelector(".total-amount");
+totalAmount.innerText=finalPrice+100;
+
 createHorizontalProductCard(cart,cartContainer,findProductInCart,"cart");
