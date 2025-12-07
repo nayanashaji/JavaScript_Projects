c20411a7399a88b1d217249f86915d0cb96fb5f9
nayanashaji <nayanashaji17102005@gmail.com>
2025-12-07 12:30:13 +0530

    Stop Clock: Added stop functionality

--- stopclock/index.js
+++ stopclock/index.js
@@ -2,13 +2,14 @@ let buttonContainer=document.querySelector(".buttonContainer");
 let clockContainer=document.querySelector(".clock");
 
 let sec=0,min=0,hour=0;
+let timerid;
 
 function buttonClick(e)
 {
     let button=e.target.name;
     if(button==="start")
     {
-        setInterval(()=>{
+        timerid=setInterval(()=>{
             sec++
             if(sec>=59)
             {
@@ -21,7 +22,11 @@ function buttonClick(e)
                 hour++;
             }
             clockContainer.innerText=`${hour<10?`0${hour}`:hour}:${min<10?`0${min}`:min}:${sec<10?`0${sec}`:sec}`
-        },100);
+        },500);
+    }
+    if(button==="stop")
+    {
+        clearInterval(timerid);
     }
 }
 
